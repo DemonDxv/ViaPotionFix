3ce8c9f57ad74160db95a5d5a088b59dc91cf4cd
DemonDxv <55443927+DemonDxv@users.noreply.github.com>
2026-03-18 05:49:50 -0500

    OOPS LOL

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    java
+}
+
+group = "dev.demon"
+version = "1.0-RELEASE"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+
+    maven("https://repo.viaversion.com")
+
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+
+    maven("https://repo.codemc.io/repository/maven-releases/")
+    maven("https://repo.codemc.io/repository/maven-snapshots/")
+}
+
+dependencies {
+    // using Spigot 1.8.8's API for reference
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+
+    compileOnly("com.github.retrooper:packetevents-spigot:2.11.2")
+    compileOnly("com.viaversion:viaversion-api:4.0.0")
+
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+    }
+
+    wrapper {
+        version = "8.14.3"
+    }
+}
